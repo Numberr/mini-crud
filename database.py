672b36a67eeaa301bf140d6970fd5b74525a606e
Numberr <myazinalex@bk.ru>
2026-04-17 12:45:44 +0500

    fix database.py

--- database.py
+++ database.py
@@ -1,16 +1,21 @@
 import psycopg2
-from dotenv import load_dotenv
 import os
+from dotenv import load_dotenv
+from contextlib import contextmanager
 
 
 load_dotenv()
 
+@contextmanager
 def get_conn():
-    conn = psycopg2.connect(
-        host=os.getenv("HOST"),
-        port=os.getenv("PORT"),
-        database=os.getenv("DATABASE"),
-        user=os.getenv("USER"),
-        password=os.getenv("PASSWORD")
-    )
-    return conn
+    try:
+        conn = psycopg2.connect(
+            host=os.getenv("HOST"),
+            port=os.getenv("PORT"),
+            database=os.getenv("DATABASE"),
+            user=os.getenv("USER"),
+            password=os.getenv("PASSWORD")
+        )
+        yield conn
+    finally:
+        conn.close()
